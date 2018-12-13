16b5246096aa42082a05f0db1665bacbc70ee155
Daniel Peixoto <danielspeixoto@outlook.com>
2018-12-13 16:19:31 -0300

    release

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"deffish-server/src/data"
 	"deffish-server/src/presentation"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("could no parse %s to port", os.Args[1])
+		panic(err)
+	}
 	log.Printf("Main started")
 	presentation.NewHandler(data.NewMongoQuestionRepository(
 		"mongodb://192.168.0.11:27017",
 		"deffishtest",
-		"questions"), 5000)
+		"questions"), port)
 }
