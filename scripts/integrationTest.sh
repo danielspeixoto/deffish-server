c6bda8f7db4ab24ff7042bd6ae9707e3bca5a6ce
Daniel Peixoto <danielspeixoto@Daniels-iMac.local>
2019-03-19 23:45:16 -0300

    auto closes execution when test finishes

--- scripts/integrationTest.sh
+++ scripts/integrationTest.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 docker rm --force -v deffishdb
-docker run -d -p 27017:27017 --name deffishdb mongo && \
-sleep 10 && \
-./scripts/local.sh & \
-sleep 20 && \
-go test -count=1 ./test/... && \
-docker rm --force -v deffishdb && \
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+docker run -d -p 27017:27017 --name deffishdb mongo
+sleep 20
+export PORT=5000
+export MONGO_CONNECTION=mongodb://localhost:27017
+export MONGO_DB_NAME=deffishtest
+
+go build main.go
+./main & RUNNING_PID=$!
+sleep 30
+go test -count=1 ./test/...
+docker rm --force -v deffishdb
+kill ${RUNNING_PID}
+
+
 
