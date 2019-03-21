3dddec54b67a715e01c5ac48132dc5e8a8d42d41
Daniel Peixoto <danielspeixoto@outlook.com>
2019-03-20 22:26:06 -0300

    Deploy

--- src/presentation/Router.go
+++ src/presentation/Router.go
@@ -12,7 +12,7 @@ type IRouter interface {
 func NewRouter(topics IRouter, essays IRouter, status IRouter, questions IRouter, port int) {
 	router := gin.Default()
 	status.Route(router.Group("/"))
-	questions.Route(router.Group("/question"))
+	questions.Route(router.Group("/questions"))
 	essays.Route(router.Group("/essay"))
 	topics.Route(router.Group("/topic"))
 	err := router.Run(":" + strconv.Itoa(port))
